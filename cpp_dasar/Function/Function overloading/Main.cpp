8d229deadcf464beeb2660a137b9291a8e2232ab
JunjungHasudungan <junjunghasudungan43@gmail.com>
2021-02-16 16:35:05 +0700

    update

--- cpp_dasar/Function/Function overloading/Main.cpp
+++ cpp_dasar/Function/Function overloading/Main.cpp
@@ -1,21 +1,43 @@
 #include <iostream>
 
 using namespace std;
+// create signiture function for other class
 
-	int tambah(int a, int b){
-		int c;
-		c = a + b;
-		return c;
-		
-	}
+	double perkalianKotak(double a, double b);
+	int perkalianKotak(int c);
+	int perkalianKotak(int d, int e, int f);
 int main(){
-	int inputNilaiA, inputNilaiB, total;
-	cout << "Masukkan nilai a :";
-	cin >> inputNilaiA;
-	cout << "Masukkan nilai b :";
-	cin >> inputNilaiB;
-	total = tambah(inputNilaiA, inputNilaiB);
-	cout << "Hasil: " << total <<endl;
+	double inputA, inputB, inputC, inputD, inputE, inputF;
 	
+	cout << "\t\t === Belajar mengunakan function overloading ===\n" << endl;
+	cout << "Masukkan Nilai A: "; 
+	cin  >> inputA;
+	cout << "Masukkan Nilai B: ";
+	cin >> inputB;
+	cout << "Hasil Perkalian dari " << inputA << " * " << inputB << " = " << perkalianKotak(inputA, inputB) <<"\n"<< endl;
+	cout << "Masukkan Nilai C: ";
+	cin >> inputC;
+	cout << "Hasil Perkalian dari " << inputC << " * " << inputC << " = " << perkalianKotak(inputC) <<"\n"<< endl;
+	cout << "Masukkan nilai D: ";
+	cin >> inputD;
+	cout << "Masukkan nilai E: ";
+	cin >> inputE;
+	cout << "Masukkan niali F: ";
+	cin >> inputF;
+	cout <<"Hasil Perkalian dari " << inputD << " * " << inputE << " * " << inputF << " = " << perkalianKotak(inputD, inputE, inputF) << endl;;
 	return 0;
 }
+
+// declaration our function before use in main 
+
+	double perkalianKotak(double a, double b){
+		return  a * b;		
+	}
+
+	int perkalianKotak(int a){
+		return a * a;
+	}
+
+	int perkalianKotak(int d, int e, int f){
+		return d * e * f;
+	}
